e2b082cd73d9868714e8c4966f7933cb0cc0913b
lishaojie <shaojieli.nlp@gmail.com>
2024-08-16 07:03:46 +0000

    process image and text

---
+++ processing_paligemma.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+# Copyright (c) 2024 Shaojie Li (shaojieli.nlp@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Optional, Tuple
+
+import numpy as np
+import torch
+from PIL import Image
+
+IMAGENET_STANDARD_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STANDARD_STD = [0.229, 0.224, 0.225]
+
+
+def add_image_tokens_to_prompt(prefix_prompt, bos_token, image_seq_len, image_token):
+    # Quoting from the blog (https://huggingface.co/blog/paligemma#detailed-inference-process):
+    #   The input text is tokenized normally.
+    #   A <bos> token is added at the beginning, and an additional newline token (\n) is appended.
+    #   This newline token is an essential part of the input prompt the model was trained with, so adding it explicitly ensures it's always there.
+    #   The tokenized text is also prefixed with a fixed number of <image> tokens.
+    # NOTE: from the paper it looks like the `\n` should be tokenized separately, but in the HF implementation this is not done.
+    #       ref to HF implementation: https://github.com/huggingface/transformers/blob/7f79a97399bb52aad8460e1da2f36577d5dccfed/src/transformers/models/paligemma/processing_paligemma.py#L55-L73
+    return f"{image_token * image_seq_len}{bos_token}{prefix_prompt}\n"
+
+
+def resize(
+    image: Image,
+    size: Tuple[int, int],
+    resample: Image.Resampling = None,
+    reducing_gap: Optional[int] = None,
+) -> np.ndarray:
+    height, width = size
+    resized_image = image.resize(
+        (width, height), resample=resample, reducing_gap=reducing_gap
+    )
+    return resized_image
+
+
+def rescale(
+    image: np.ndarray, scale: float, dtype: np.dtype = np.float32
+) -> np.ndarray:
+    rescaled_image = image * scale
+    rescaled_image = rescaled_image.astype(dtype)
+    return rescaled_image
+
+
+def normalize(
+    image: np.ndarray, mean: Union[float, List[float]], std: Union[float, List[float]]
+) -> np.ndarray:
+    mean = np.array(mean)
+    std = np.array(std)
+    image = (image - mean) / std
+    return image
+
+
+def process_images(
+    images: List[Image.Image],
+    size: Tuple[int, int] = None,
+    resample: Image.Resampling = None,
+    rescale_factor: float = None,
+    image_mean: Optional[Union[float, List[float]]] = None,
+    image_std: Optional[Union[float, List[float]]] = None,
+) -> List[np.ndarray]:
+
+    height, weight = size[0], size[1]
+    images = [
+        resize(image=image, size=(height, weight), resample=resample)
+        for image in images
+    ]
+    images = [np.array(image) for image in images]
+    images = [rescale(image=image, scale=rescale_factor) for image in images]
+    images = [
+        normalize(image=image, mean=image_mean, std=image_std) for image in images
+    ]
+    # [H, W, C] -> [C, H, W]
+    images = [image.transpose(2, 0, 1) for image in images]
+    return images
+
+
+class PaliGemmaProcessor:
+    IMAGE_TOKEN = "<image>"
+
+    def __init__(self, tokenizer, num_image_tokens: int, image_size: int):
+
+        self.image_seq_length = num_image_tokens
+        self.image_size = image_size
+
+        # Tokenizer described here: https://github.com/google-research/big_vision/blob/main/big_vision/configs/proj/paligemma/README.md#tokenizer
+        tokens_to_add = {"additional_special_tokens": [self.IMAGE_TOKEN]}
+        tokenizer.add_special_tokens(tokens_to_add)
+        EXTRA_TOKENS = [
+            f"<loc{i:04d}>" for i in range(1024)
+        ]  # These tokens are used for object detection (bounding boxes)
+        EXTRA_TOKENS += [
+            f"<seg{i:03d}>" for i in range(128)
+        ]  # These tokens are used for object segmentation
+        tokenizer.add_tokens(EXTRA_TOKENS)
+        self.image_token_id = tokenizer.convert_tokens_to_ids(self.IMAGE_TOKEN)
+        # We will add the BOS and EOS tokens ourselves
+        tokenizer.add_bos_token = False
+        tokenizer.add_eos_token = False
+
+        self.tokenizer = tokenizer
+
+    def __call__(
+        self,
+        texts: List[str],
+        images: List[Image.Image],
+        padding: str = "longest",
+        truncation: bool = True,
+    ) -> dict:
+
+        assert (
+            len(texts) == 1 and len(images) == 1
+        ), f"Only one text and one image is supported, got {len(text)} text and {len(image)} images"
+
+        pixel_values = process_images(
+            images,
+            size=(self.image_size, self.image_size),
+            resample=Image.Resampling.BICUBIC,
+            rescale_factor=1 / 255.0,
+            image_mean=IMAGENET_STANDARD_MEAN,
+            image_std=IMAGENET_STANDARD_STD,
+        )
+
+        # convert thr list of numpy arrays to a single numpy array[B, C, H, W]
+        pixel_values = np.stack(pixel_values, axis=0)
+        pixel_values = torch.tensor(pixel_values)
+
+        # prepend the image token to the text
+
+        input_strings = [
+            add_image_tokens_to_text(
+                prefix_prompt=prompt,
+                bos_token=self.tokenizer.bos_token,
+                image_seq_len=self.image_seq_length,
+                image_token=self.IMAGE_TOKEN,
+            )
+            for prompt in texts
+        ]
+
+        # tokenize the input
+        inputs = self.tokenizer(
+            input_strings, padding=padding, truncation=truncation, return_tensors="pt"
+        )
+
+        return {"pixel_values": pixel_values, **inputs}
